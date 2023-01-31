466722aa36fac21bdb2bc6276f9a9245936e18e7
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-01-31 23:09:43 +0100

    feat:added control structuresa and loops

--- Introduction-to-C-sharp/Program.cs
+++ Introduction-to-C-sharp/Program.cs
@@ -20,6 +20,39 @@ namespace HelloWorld
             Console.WriteLine(Days.Sun);
             int val1 = 34, val2 = 45;
             Console.WriteLine(val1 > val2);
+            if (val1 > val2) {
+                Console.WriteLine("val1 is greater than val2");
+            }
+            else {
+                Console.WriteLine("val1 is less than val2");
+            }
+            int value = 89;
+            switch(value){
+                case 1: Console.WriteLine("Value is 1");
+                    break;
+                case 2:Console.WriteLine("Value is 2 ");
+                    break;
+                default:Console.WriteLine("Value is different");
+                    break; 
+            }
+            int val = 4,i = 0;
+            while(i < val){
+                Console.WriteLine(i);
+                i += 1;
+            }
+            for (int j = 0; i < 7; i++)
+            {
+                Console.WriteLine(j);
+            }
+            int[] values;
+
+            values = new int[4];
+            values[0] = 1;
+            values[1] = 2;
+            values[2] = 3;
+            Console.WriteLine(values[0]);
+            Console.WriteLine(values[1]);
+            Console.WriteLine(values[2]);
         } 
     }
 }
