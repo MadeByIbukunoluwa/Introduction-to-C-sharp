97825171362ff149759939da3c0a47970877736b
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-01-31 22:31:35 +0100

    feat:added datatypes in C#

--- Introduction-to-C-sharp/Program.cs
+++ Introduction-to-C-sharp/Program.cs
@@ -1,5 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        enum Days {Sun,Mon,Tue,Wed,Thu,Fri,Sat}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            //Console.ReadLine();
+            int num = 30;
+            Console.WriteLine(num);
+            double numDouble = 56.92;
+            Console.WriteLine(numDouble);
+            bool status = true;
+            Console.WriteLine(status);
+            string message = "Hello";
+            Console.WriteLine(message);
+            Console.WriteLine(Days.Sun);
+            int val1 = 34, val2 = 45;
+            Console.WriteLine(val1 > val2);
+        } 
+    }
+}
